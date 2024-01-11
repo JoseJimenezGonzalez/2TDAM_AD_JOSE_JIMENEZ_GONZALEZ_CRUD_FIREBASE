59720fd0f9b30466e8b2be1e8abf5e065c3fff80
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-11 16:30:45 +0100

    commit ok

--- app/src/main/java/com/example/accesodatos/EditarJuegoActivity.kt
+++ app/src/main/java/com/example/accesodatos/EditarJuegoActivity.kt
@@ -3,6 +3,8 @@ package com.example.accesodatos
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.bumptech.glide.Glide
 import com.example.accesodatos.databinding.ActivityEditarJuegoBinding
 import com.google.firebase.database.DatabaseReference
@@ -23,6 +25,43 @@ class EditarJuegoActivity : AppCompatActivity() {
 
     private lateinit var job: Job
 
+    private val opcionesGeneroJuegos = arrayOf(
+        "Acción",
+        "Aventura",
+        "Disparos en primera persona",
+        "Disparos en tercera persona",
+        "Sigilo",
+        "Deportes",
+        "Estrategia en tiempo real",
+        "Estrategia por turnos",
+        "Rol",
+        "Simulación de vida",
+        "Simulación de construcción",
+        "Simulación de negocios",
+        "Simulación de vuelo",
+        "Simulación de conducción",
+        "Simulación deportiva",
+        "Carreras",
+        "Lucha",
+        "Arcade",
+        "Plataforma",
+        "Puzles",
+        "Horror",
+        "Survival Horror",
+        "Mundo abierto",
+        "Multijugador masivo en línea",
+        "Música y ritmo",
+        "Educativo",
+        "Familiar",
+    )
+    private val edadesRecomendadasJuegos = arrayOf(
+        "Para todos (E)",
+        "Mayores de 10 años (E10+)",
+        "Mayores de 13 años (T)",
+        "Mayores de 17 años (M)",
+        "Adultos (AO)",
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,6 +75,31 @@ class EditarJuegoActivity : AppCompatActivity() {
 
         //Voy solo a por el nombre en principio
         binding.tietNombreJuego.setText(pojoJuego.nombre)
+        binding.tietNombreEstudio.setText(pojoJuego.estudio)
+        binding.tietFechaLanzamiento.setText(pojoJuego.fechaLanzamiento)
+        //Para los spinner tengo que obtener la posicion que ocupa
+        //Posicion que ocupa la edad recomendada para jugar al juego
+        var posicionEdadRecomendada = edadesRecomendadasJuegos.indexOf(pojoJuego.edad)
+        var posicionGeneroDelJuego = opcionesGeneroJuegos.indexOf(pojoJuego.genero)
+        //Ahora vamos con los spinner
+        //Configurar el spinner
+        //Obtenemos el spinner edades
+        val spinnerEdades = findViewById<Spinner>(R.id.spinnerEdad)
+        // Crear un ArrayAdapter utilizando la lista de opciones y el diseño predeterminado
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, edadesRecomendadasJuegos)
+        // Aplicar el adaptador al Spinner
+        binding.spinnerEdad.adapter = adapter
+        // Recuperar la posición guardada y establecerla en el Spinner
+        binding.spinnerEdad.setSelection(posicionEdadRecomendada)
+
+        //Obtenemos el spinner generos de los juegos
+        val spinnerGeneros = findViewById<Spinner>(R.id.spinnerGenero)
+        // Crear un ArrayAdapter utilizando la lista de opciones y el diseño predeterminado
+        val adapterGenero = ArrayAdapter(this, android.R.layout.simple_list_item_1, opcionesGeneroJuegos)
+        // Aplicar el adaptador al Spinner
+        binding.spinnerGenero.adapter = adapterGenero
+        // Recuperar la posición guardada y establecerla en el Spinner
+        binding.spinnerGenero.setSelection(posicionGeneroDelJuego)
 
         Glide.with(applicationContext)
             .load(pojoJuego.imagen)
@@ -47,5 +111,9 @@ class EditarJuegoActivity : AppCompatActivity() {
         stRef = FirebaseStorage.getInstance().getReference()
 
         listaJuegos = Utilidades.obtenerListaJuegos(dbRef)
+
+        binding.btnIntroducirJuego.setOnClickListener {
+
+        }
     }
 }
